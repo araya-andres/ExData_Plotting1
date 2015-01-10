14c173a37add9856b6744c919e049430b67ff40e
Andres Araya <araya.andres@gmail.com>
2015-01-10 11:09:19 -0600

    Typo fixed. Rearranging statements.

--- plot1.R
+++ plot1.R
@@ -1,17 +1,13 @@
 fname <- 'data//household_power_consumption.txt'
 output <- 'plot1.png'
-
-power <- read.table(fname, header = T, sep = ';', stringsAsFactors = F)
-
-power <- cbind(power,
-               'Date/Time'=strptime(paste(power$Date, power$Time), '%d/%m/%Y %H:%M:%S'))
-
 startDate <- as.POSIXlt('2007-02-01')
 endDate <- as.POSIXlt('2007-02-03')
 
-power <- power[which(power$'Date/Time' >= startDate),]
-power <- power[which(power$'Date/Time' <= endDate),]
-
+power <- read.table(fname, header = T, sep = ';', stringsAsFactors = F)
+power <- cbind(power,
+               'Date/Time'=strptime(paste(power$Date, power$Time), '%d/%m/%Y %H:%M:%S'))
+power <- power[which(power$'Date/Time' >= startDate), ]
+power <- power[which(power$'Date/Time' <= endDate), ]
 power$Global_active_power <- as.numeric(power$Global_active_power)
 
 hist(power$Global_active_power, 
@@ -21,4 +17,3 @@ hist(power$Global_active_power,
 
 dev.copy(png, file = output)
 dev.off()
-sour
